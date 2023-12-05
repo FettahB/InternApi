1e934f2021f9b7e4ca8ccc7f85520fe7dcabc9aa
Muhammed Fettah Başdemir <129119010+FettahB@users.noreply.github.com>
2023-12-05 22:18:36 +0300

    Init
    Init

---
+++ InternApi/InternApi/Database/Context/MyDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InternApi.Database.Context
+{
+    public class MyDbContext
+    {
+        public MyDbContext(DbContextOptions options)
+        {
+
+        }
+
+    }
+}
